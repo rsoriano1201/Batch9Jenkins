edae3c1b850bd4a02c0fe5e7d1f9029431b33727
rsoriano1201 <rsoriano1201@gmail.com>
2021-07-23 03:29:59 -0700

    First commit

---
+++ src/com/collection/Main.java
@@ -0,0 +1,13 @@
+package com.collection;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("batch 9 is great");
+            Thread.sleep(1000);
+
+        }
+    }
+}
